4e401019b62682a4e864225450d718c767e298c2
kb4ek <kbaek1202@gmail.com>
2019-04-21 01:32:59 +0900

    view page

--- routes/apiController.js
+++ routes/apiController.js
@@ -1,10 +1,17 @@
 const router = require('express').Router();
 
 // Request reviews, Review comments
-const RequestReview = require('./middleware/post/RequestReview');
-const ReviewComment = require('./middleware/post/ReviewComment');
+const RequestReview = require('./controller/RequestReview');
+const ReviewComment = require('./controller/ReviewComment');
 
+// view Posts
+const viewPost = require('./middleware/post/viewPost');
+
+// Request and review routers
 router.post('/request', RequestReview);
 router.post('/review', ReviewComment);
+
+// method type : get | view page
+router.get('/view', viewPost);
 
 module.exports = router;
